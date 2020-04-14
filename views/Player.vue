dcc4a8feecc6cb5cacae4806ca2fd7702ee5a98c
Jose Cisneros <josecis0216@gmail.com>
2020-04-14 10:22:16 -0600

    added views for new routes

---
+++ views/Player.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Player/>
+    </div>
+</template>
+
+<script>
+import Player from "@/components/Player.vue"
+
+export default {
+    name: "player",
+    components: {
+        Player
+    }
+}
+</script>
